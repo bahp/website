34517c40ecce3f0b8f064528931e8c387ea0d229
bahp <bernard.hernandez.perez@gmail.com>
2025-11-18 09:59:57 +0000

    Deploying to gh-pages from @ bahp/website@5abaf192384a5b090affe629c06d496b2d3eb467 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your projects.",
+          description: "A growing collection of projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/website/projects/";
